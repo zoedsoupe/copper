b3229781d5b8c394804527ec2cf162fedeed853e
matdsoupe <matheus_pessanha2001@outlook.com>
2021-08-20 14:33:15 -0300

    chore: try to load correct theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,7 +1,8 @@
+local global_theme = vim.g.theme
 local present, base16 = pcall(require, "base16")
 
 if present then
-   base16(base16.themes(chad_theme), true)
+   base16(base16.themes(global_theme), true)
    require("highlights")
    return true
 else
