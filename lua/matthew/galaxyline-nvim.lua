4e4004a0ce468fd11b549df4e19e96d2db9f1cd1
matdsoupe <matheus_pessanha2001@outlook.com>
2021-08-20 11:00:51 -0300

    fix: temp solution for `vim.g.theme`

--- lua/matthew/galaxyline-nvim.lua
+++ lua/matthew/galaxyline-nvim.lua
@@ -5,7 +5,7 @@ local gls = gl.section
 
 gl.short_line_list = {'NvimTree','vista','dbui'}
 
-local global_theme = "themes/" .. vim.g.theme
+local global_theme = "themes/" .. (vim.g.theme or 'onedark')
 local colors = require(global_theme)
 
 local buffer_not_empty = function()
