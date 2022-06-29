f68be59dbecefcf974d8ad60a39adc45e8a7c424
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-06-29 15:24:03 -0300

    feat: get plugins names and only use one system

--- flake.nix
+++ flake.nix
@@ -1,178 +1,307 @@
 {
-  description = "Math's neovim config";
+  description = "Zoey's neovim config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
 
-    neovim = {
-      url = "github:neovim/neovim?dir=contrib";
+    neovim-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
 
-    vim-rescript = { url = "github:rescript-lang/vim-rescript"; flake = false; };
-    nvim-treesitter-rescript = { url = "github:nkrkv/nvim-treesitter-rescript"; flake = false; };
-    editorconfig-vim = { url = "github:editorconfig/editorconfig-vim"; flake = false; };
-    true-zen = { url = "github:Pocco81/TrueZen.nvim"; flake = false; };
-    emmet-vim = { url = "github:mattn/emmet-vim"; flake = false; };
-    nvim-ts-rainbow = { url = "github:p00f/nvim-ts-rainbow"; flake = false; };
-    codi-vim = { url = "github:metakirby5/codi.vim"; flake = false; };
-    vim-surround = { url = "github:tpope/vim-surround"; flake = false; };
-    vimtex = { url = "github:lervag/vimtex"; flake = false; };
-    direnv-vim = { url = "github:direnv/direnv.vim"; flake = false; };
-    vim-matchup = { url = "github:andymass/vim-matchup"; flake = false; };
-    nvim-comment = { url = "github:terrortylor/nvim-comment"; flake = false; };
-    neoscroll = { url = "github:karb94/neoscroll.nvim"; flake = false; };
-    bullets-vim = { url = "github:dkarter/bullets.vim"; flake = false; };
-    telescope-nvim = { url = "github:nvim-telescope/telescope.nvim"; flake = false; };
-    vim-highlightedyank = { url = "github:machakann/vim-highlightedyank"; flake = false; };
-    nvim-colorizer-lua = { url = "github:norcalli/nvim-colorizer.lua"; flake = false; };
-    dashboard-nvim = { url = "github:glepnir/dashboard-nvim"; flake = false; };
-    nvim-autopairs = { url = "github:windwp/nvim-autopairs"; flake = false; };
-    vim-haskell-module-name = { url = "github:UnkindPartition/vim-hs-module-name"; flake = false; };
-    indent-blankline-nvim-lua = { url = "github:lukas-reineke/indent-blankline.nvim"; flake = false; };
-    monochrome = { url = "github:kdheepak/monochrome.nvim"; flake = false; };
-    neon = { url = "github:rafamadriz/neon"; flake = false; };
-    vim-polyglot = { url = "github:sheerun/vim-polyglot"; flake = false; };
-    gitsigns-nvim = { url = "github:lewis6991/gitsigns.nvim"; flake = false; };
-    nvim-treesitter = { url = "github:nvim-treesitter/nvim-treesitter"; flake = false; };
-    nvim-tree-lua = { url = "github:kyazdani42/nvim-tree.lua"; flake = false; };
-    galaxyline-nvim = { url = "github:glepnir/galaxyline.nvim"; flake = false; };
-    popup-nvim = { url = "github:nvim-lua/popup.nvim"; flake = false; };
-    plenary-nvim = { url = "github:nvim-lua/plenary.nvim"; flake = false; };
-    nvim-web-devicons = { url = "github:kyazdani42/nvim-web-devicons"; flake = false; };
-    vimagit = { url = "github:jreybert/vimagit"; flake = false; };
-    nvim-which-key = { url = "github:folke/which-key.nvim"; flake = false; };
-    syntastic = { url = "github:vim-syntastic/syntastic"; flake = false; };
-    trouble = { url = "github:folke/trouble.nvim"; flake = false; };
-    vim-elixir = { url = "github:elixir-editors/vim-elixir"; flake = false; };
-    calvera-dark = { url = "github:yashguptaz/calvera-dark.nvim"; flake = false; };
-    sonokai = { url = "github:sainnhe/sonokai"; flake = false; };
-    earthly-vim = { url = "github:earthly/earthly.vim"; flake = false; };
-    material-nvim = { url = "github:marko-cerovac/material.nvim"; flake = false; };
-    nvim-lspconfig = { url = "github:neovim/nvim-lspconfig"; flake = false; };
-    "coq.artifacts" = { url = "github:ms-jpq/coq.artifacts"; flake = false; };
+    # LSP plugins
+    coq-nvim = {
+      url = "github:ms-jpq/coq_nvim";
+      flake = false;
+    };
+
+    coq-artifacts = {
+      url = "github:ms-jpq/coq.artifacts";
+      flake = false;
+    };
+
+    nvim-lspconfig = {
+      url = "github:neovim/nvim-lspconfig";
+      flake = false;
+    };
+
+    lspsaga = {
+      url = "github:tami5/lspsaga.nvim";
+      flake = false;
+    };
+
+    lspkind = {
+      url = "github:onsails/lspkind-nvim";
+      flake = false;
+    };
+
+    trouble = {
+      url = "github:folke/trouble.nvim";
+      flake = false;
+    };
+
+    nvim-code-action-menu = {
+      url = "github:weilbith/nvim-code-action-menu";
+      flake = false;
+    };
+
+    lsp-signature = {
+      url = "github:ray-x/lsp_signature.nvim";
+      flake = false;
+    };
+
+    null-ls = {
+      url = "github:jose-elias-alvarez/null-ls.nvim";
+      flake = false;
+    };
+
+    rust-tools = {
+      url = "github:simrat39/rust-tools.nvim";
+      flake = false;
+    };
+
+    rnix-lsp.url = "github:nix-community/rnix-lsp";
+
+    # Telescope
+    telescope-nvim = {
+      url = "github:nvim-telescope/telescope.nvim";
+      flake = false;
+    };
+
+    telescope-ui-select = {
+      url = "github:nvim-telescope/telescope-ui-select.nvim";
+      flake = false;
+    };
+
+    # Registers
+    nvim-neoclip = {
+      url = "github:AckslD/nvim-neoclip.lua";
+      flake = false;
+    };
+
+    # Treesitter
+    nvim-treesitter = {
+      url = "github:nvim-treesitter/nvim-treesitter";
+      flake = false;
+    };
+
+    nvim-treesitter-rescript = {
+      url = "github:nkrkv/nvim-treesitter-rescript";
+      flake = false;
+    };
+
+    # Language specific
+    earthly-vim = {
+      url = "github:earthly/earthly.vim";
+      flake = false;
+    };
+
+    vim-rescript = {
+      url = "github:rescript-lang/vim-rescript";
+      flake = false;
+    };
+
+    editorconfig-vim = {
+      url = "github:editorconfig/editorconfig-vim";
+      flake = false;
+    };
+
+    vim-haskell-module-name = {
+      url = "github:UnkindPartition/vim-hs-module-name";
+      flake = false;
+    };
+
+    vim-polyglot = {
+      url = "github:sheerun/vim-polyglot";
+      flake = false;
+    };
+
+    emmet-vim = {
+      url = "github:mattn/emmet-vim";
+      flake = false;
+    };
+
+    vimtex = {
+      url = "github:lervag/vimtex";
+      flake = false;
+    };
+
+    direnv-vim = {
+      url = "github:direnv/direnv.vim";
+      flake = false;
+    };
+
+    vim-elixir = {
+      url = "github:elixir-editors/vim-elixir";
+      flake = false;
+    };
+
+    # Buffer tools
+    bufdelete-nvim = {
+      url = "github:famiu/bufdelete.nvim";
+      flake = false;
+    };
+
+    # Visual
+    nvim-web-devicons = {
+      url = "github:kyazdani42/nvim-web-devicons";
+      flake = false;
+    };
+
+    gitsigns-nvim = {
+      url = "github:lewis6991/gitsigns.nvim";
+      flake = false;
+    };
+
+    indent-blankline-nvim-lua = {
+      url = "github:lukas-reineke/indent-blankline.nvim";
+      flake = false;
+    };
+
+    true-zen = {
+      url = "github:Pocco81/TrueZen.nvim";
+      flake = false;
+    };
+
+    nvim-ts-rainbow = {
+      url = "github:p00f/nvim-ts-rainbow";
+      flake = false;
+    };
+
+    vim-highlightedyank = {
+      url = "github:machakann/vim-highlightedyank";
+      flake = false;
+    };
+
+    neoscroll = {
+      url = "github:karb94/neoscroll.nvim";
+      flake = false;
+    };
+
+    nvim-colorizer-lua = {
+      url = "github:norcalli/nvim-colorizer.lua";
+      flake = false;
+    };
+
+    dashboard-nvim = {
+      url = "github:glepnir/dashboard-nvim";
+      flake = false;
+    };
+
+    nvim-autopairs = {
+      url = "github:windwp/nvim-autopairs";
+      flake = false;
+    };
+
+    # Themes
+    material-nvim = {
+      url = "github:marko-cerovac/material.nvim";
+      flake = false;
+    };
+
+    # Extra
+    plenary-nvim = {
+      url = "github:nvim-lua/plenary.nvim";
+      flake = false;
+    };
+
+    codi-vim = {
+      url = "github:metakirby5/codi.vim";
+      flake = false;
+    };
+
+    surround-nvim = {
+      url = "github:ur4ltz/surround.nvim";
+      flake = false;
+    };
+
+    vim-matchup = {
+      url = "github:andymass/vim-matchup";
+      flake = false;
+    };
+
+    nvim-comment = {
+      url = "github:terrortylor/nvim-comment";
+      flake = false;
+    };
+
+    bullets-vim = {
+      url = "github:dkarter/bullets.vim";
+      flake = false;
+    };
+
+    nvim-tree-lua = {
+      url = "github:kyazdani42/nvim-tree.lua";
+      flake = false;
+    };
+
+    galaxyline-nvim = {
+      url = "github:glepnir/galaxyline.nvim";
+      flake = false;
+    };
+
+    popup-nvim = {
+      url = "github:nvim-lua/popup.nvim";
+      flake = false;
+    };
+
+    vimagit = {
+      url = "github:jreybert/vimagit";
+      flake = false;
+    };
+
+    nvim-which-key = {
+      url = "github:folke/which-key.nvim";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, neovim, rust-overlay, ... }@inputs:
-  let
-    plugins = [
-      "vim-rescript"
-      "nvim-treesitter-rescript"
-      "editorconfig-vim"
-      "true-zen"
-      "emmet-vim"
-      "nvim-ts-rainbow"
-      "codi-vim"
-      "vim-surround"
-      "vimtex"
-      "direnv-vim"
-      "ultisnips"
-      "vim-matchup"
-      "vim-snippets"
-      "nvim-comment"
-      "popup-nvim"
-      "plenary-nvim"
-      "neoscroll"
-      "bullets-vim"
-      "telescope-nvim"
-      "vim-highlightedyank"
-      "nvim-colorizer-lua"
-      "dashboard-nvim"
-      "nvim-autopairs"
-      "vim-haskell-module-name"
-      "indent-blankline-nvim-lua"
-      "monochrome"
-      "neon"
-      "vim-polyglot"
-      "gitsigns-nvim"
-      "nvim-treesitter"
-      "nvim-tree-lua"
-      "galaxyline-nvim"
-      "nvim-web-devicons"
-      "vimagit"
-      "nvim-which-key"
-      "syntastic"
-      "trouble"
-      "vim-elixir"
-      "calvera-dark"
-      "sonokai"
-      "earthly-vim"
-      "material-nvim"
-      "nvim-lspconfig"
-      "coq.artifacts"
-    ];
-
-    externalOverlay = prev: super: {
-      neovim-nightly = neovim.defaultPackage.${prev.system};
-    };
-
-    pluginOverlay = prev: super: let
-      buildPlug = name: prev.vimUtils.buildVimPluginFrom2Nix {
-        pname = name;
-        version = "HEAD";
-        src = builtins.getAttr name inputs;
-      };
-    in {
-      neovimPlugins = builtins.listToAttrs (map (name: { inherit name; value = buildPlug name; }) plugins);
-    };
-
-    allPkgs = lib.mkPkgs {
-      inherit nixpkgs;
-      cfg = { };
-      overlays = [
-        pluginOverlay
-        externalOverlay
-      ];
-    };
-
-    lib = import ./lib;
-
-    mkNeovimPkg = pkgs: lib.neovimWrapper {
-      inherit pkgs;
-      config.vim = {
-        viAlias = true;
-        vimAlias = true;
-        lsp.enable = true;
-        theme.material.enable = true;
-        disableArrows = true;
-        editor.indentGuide = true;
-        trouble.enable = false;
-      };
-    };
-
-    overlays = [ (import rust-overlay) ];
-
-    pkgs = import nixpkgs {
-      inherit overlays;
+    let
       system = "x86_64-linux";
-    };
-  in {
-    apps = lib.withDefaultSystems (sys: {
-      nvim = {
-        type = "app";
-        program = "${self.defaultPackage."${sys}"}/bin/nvim";
+
+      lib = import ./lib.nix { inherit pkgs inputs; };
+
+      inherit (import ./overlays.nix {
+        inherit system inputs lib;
+      }) overlays;
+
+      pkgs = import nixpkgs {
+        inherit system overlays;
+
+        config.allowUnfree = true;
       };
-    });
+    in
+    rec {
+      inherit (lib) mkNeovim;
 
-    defaultApp = lib.withDefaultSystems (sys: {
-      type = "app";
-      program = "${self.defaultPackage."${sys}"}/bin/nvim";
-    });
+      apps."${system}" = rec {
+        default = nvim;
 
-    defaultPackage = lib.withDefaultSystems (sys: self.packages."${sys}".copper);
+        nvim = {
+          type = "app";
+          programs = "${packages."${system}".default}/bin/nvim";
+        };
+      };
 
-    packages = lib.withDefaultSystems (sys: {
-      copper = mkNeovimPkg allPkgs."${sys}";
-    });
+      devShells."${system}".default = pkgs.mkShell {
+        buildInputs = [ packages "${system}".copper ];
+      };
 
-    devShell = pkgs.mkShell {
-      name = "copper";
-      buildInputs = with pkgs; [
-        rnix-lsp elixir_ls rust-bin.stable.latest.rls
-      ];
+      overlays.default = super: self: {
+        inherit mkNeovim;
+        inherit (pkgs) neovimPlugins;
+
+        copper = packages."${system}".copper;
+      };
+
+      packages."${system}" = rec {
+        default = copper;
+        copper = mkNeovim {
+          config = { };
+        };
+      };
     };
-  };
 }
---
+++ lib.nix
@@ -0,0 +1,91 @@
+{ pkgs, inputs, ... }:
+
+{
+  inherit (pkgs.lib);
+
+  mkNeovim = { config }:
+    let
+      plugins = pkgs.neovimPlugins;
+
+      vimOpts = lib.evalModule {
+        modules = [
+          { imports = [ ../modules ]; }
+          config
+        ];
+
+        specialArgs = {
+          inherit pkgs;
+        };
+      };
+
+      vim = vimOpts.config.vim;
+    in
+    pkgs.wrapNeovim pkgs.neovim {
+      inherit (vim) viALias vimAlias;
+
+      configure = {
+        customRC = vim.configRC;
+
+        packages.myVimPackage = with plugins; {
+          start = builtins.filter (f: f != null) vim.startPlugins;
+          opt = vim.optPlugins;
+        };
+      };
+    };
+
+  buildPluginOverlay = super: self:
+    let
+      inherit (builtins) attrNames filter listToAttrs;
+      inherit (self.vimUtils) buildPluginFrom2Nix;
+
+      treesitterGrammars = self.tree-sitter.withPlugins (p: with p; [
+        tree-sitter-bash
+        tree-sitter-c
+        tree-sitter-commonlisp
+        tree-sitter-css
+        tree-sitter-dockerfile
+        tree-sitter-elisp
+        tree-sitter-elixir
+        tree-sitter-erlang
+        tree-sitter-fish
+        tree-sitter-haskell
+        tree-sitter-html
+        tree-sitter-javascript
+        tree-sitter-json
+        tree-sitter-latex
+        tree-sitter-lua
+        tree-sitter-nix
+        tree-sitter-python
+        tree-sitter-rust
+        tree-sitter-toml
+        tree-sitter-typescript
+        tree-sitter-vue
+        tree-sitter-yaml
+      ]);
+
+      buildPlug = name: buildPluginFrom2Nix {
+        pname = name;
+        version = "HEAD";
+        src = builtins.getAttr name inputs;
+        postPatch =
+          if (name == "nvim-treesitter")
+          then ''
+            rm -r parser
+            ln -s ${treesitterGrammars} parser
+          ''
+          else "";
+      };
+
+      isPlugin = n: n != "neovim" && n != "nixpkgs";
+
+      plugins = filter isPlugin (attrNames inputs);
+    in
+    {
+      neovimPlugins = listToAttrs
+        (map (name: {
+          inherit name;
+          value = buildPlug name;
+        }))
+        plugins;
+    };
+}
